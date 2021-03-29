fed748662aef40bdbcd261901bcf1bd745eb8914
Aymie <aymie.walid.chalouhy@gmail.com>
2021-03-29 23:42:58 +0300

    configurable save file

--- tasks.js
+++ tasks.js
@@ -9,22 +9,21 @@
  * @param  {string} name the name of the app
  * @returns {void}
  */
-
-var elements = ['quit', 'exit', 'hello', 'help', 'list', 'add', 'remove','check','uncheck'];
-var Elts = [{ name: 'quit', done: true }, { name: 'exit', done: true }, { name: 'hello', done: true },
-{ name: 'help', done: true }, { name: 'list', done: true }, { name: 'add', done: true }, { name: 'remove', done: true }];
 var elements = ['quit', 'exit', 'hello', 'help', 'list', 'add', 'remove', 'check', 'uncheck'];
 var fs = require('fs');
 var data = fs.readFileSync('database.json', 'utf-8');
 try {
   if (data == "") throw "Empty"
 }
-catch { data =' [{ "name": "coffe", "done": true }] '}
+catch { data = '[{ "name": "coffe", "done": true }] ' }
 var Elts = JSON.parse(data);
 
+const process = require('process');
+var proce = process.argv;
 
-
-function startApp(name){
+//I did it from the start add & remove 
+// sorry
+function startApp(name) {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', onDataReceived);
@@ -48,53 +47,44 @@ function startApp(name){
  * @param  {string} text data typed by the user
  * @returns {void}
  */
-
-
+console.log('write hello & your name :');
 function onDataReceived(text) {
-  text= text.replace('\n','').trim();
-  var arrayText = text.split(' ');
+  var fun = text.trim().split(" ", 1);
 
-  if (arrayText[0] === 'quit' || arrayText[0] === 'exit') {
-    quit();
+  if (text.trim() === 'quit' || text.trim() === 'exit') {
+    quit(proce);
   }
-  else if(arrayText[0] === 'hello'){
-    hello();
+  else if (text.trim() == 'list') {
+    list(Elts);
   }
-  else  if(arrayText[0] === 'help'){
-    listOfCommands();
-}
-else  if(arrayText[0] === 'list'){
-  list(elements);
-  list(Elts);
-}
-else if (text.trim().split(" ", 1) == 'add') {
-  add(elements, text);
-  add(Elts, text);
-}
-else if (text.trim().split(" ", 1) == 'remove') {
-  remove(elements, text);
-  remove(Elts, text);
-}
-else if (text.trim().split(" ", 1) == 'edit') {
-  edit(elements, text);
-  edit(Elts, text);
-}
-else if (text.trim() == 'list') {
-  list(elements);
-  list(Elts);
-}
-else if (text.trim().split(" ", 1) == 'check') {
-  check(Elts, text);
-}
-else if (text.trim().split(" ", 1) == 'uncheck') {
-  uncheck(Elts, text);
-}
-  else{
+  else if (fun == 'add') {
+    add(Elts, text);
+  }
+  else if (fun == 'remove') {
+    remove(Elts, text);
+  }
+  else if (fun == 'edit') {
+    edit(Elts, text);
+  }
+  else if (fun == 'hello') {
+    var UserName = text.trim().split(" ").pop();
+    hello(UserName);
+  }
+  else if (text.trim() === 'help' || fun == 'help') {
+    help(elements);
+  }
+  else if (fun == 'check') {
+    check(Elts, text);
+  }
+  else if (fun == 'uncheck') {
+    uncheck(Elts, text);
+  }
+  else {
     unknownCommand(text);
   }
 }
 
- 
+
 /**
  * prints "unknown command"
  * This function is supposed to run when all other commands have failed
@@ -102,8 +92,8 @@ else if (text.trim().split(" ", 1) == 'uncheck') {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"')
 }
 
 
@@ -112,133 +102,74 @@ function unknownCommand(c){
  *
  * @returns {void}
  */
-
-function hello(){
-  console.log('hello!')
+function hello(UserName) {
+  if (UserName === "hello") {
+    console.log('hello!');
+  }
+  else {
+    console.log('hello ' + UserName + '!')
+  }
 }
 
-function hello(text){
-  if (text) {
-    console.log("hello" + " " +text+ "!");
-  }
-    else{
-      console.log("hello!");
-    }
-  }
 
 /**
- * this function is made to print all the existing commands.
- * It works when you type help.
- *
+ * * lists all the possible commands
+ * 
  * @returns {void}
  */
-function listOfCommands(){
-  console.log('\n  Below are the possible commands: \n', '\n', 
-   '1- help:Shows the available commands\n\n',
-   '2- hello: Shows hello! message\n\n',
-   '3- hello and type any name: Shows hello message and the name you have entered\n\n',
-   '4- quit/exit: Quit the application\n\n',
-   '5- list: shows you all the tasks\n\n',
-   '6- add: permit you to add a new task\n\n',
-   '7- remove: remove some tasks from the list\n\n',
-   '8- Check: marked the tasks you did in the list\n\n',
-   '9- Unchek: shows you the tasks you have not Done\n\n',
-  );
+function list(Elts) {
+  console.log('List of commands :')
+  for (var i = 0; i < Elts.length; i++) {
+    if (Elts[i].done) {
+      console.log("[✓] " + Elts[i].name)
+    }
+    else {
+      console.log("[ ] " + Elts[i].name)
+    }
+  }
 }
+
 
 /**
- * Exits the application
- *
+ * * add
+ * 
  * @returns {void}
  */
-function quit() {
-  var fs = require('fs');
-  var data = JSON.stringify(Elts);
-  fs.writeFileSync('database.json', data);
-  console.log('data saved successfully')
-  console.log('Quitting now, goodbye!')
-  process.exit();
-}
-function exit(){
-  console.log('Quitting now, goodbye!')
-  process.exit();
-}
-
-var tasks = ['Testing the software' , 'Little steps' , 'String manipulation',
-'Additional commands',' Refinements','Data modelling','Persistent data'];
-
-function list() {
-  for (var i = 0; i < tasks.length; i++) {
-    console.log(i + 1 + " - " + tasks[i]);
+function add(Elts, text) {
+  var task = text.trim().split(" ").pop();
+  if (text.trim().split(" ").length == 1) {
+    console.log('"error" no tasks to add!')
+  } else {
+    Elts = Elts.push({ name: task, done: false });
+    console.log(task + ' has been added to list successfully.')
   }
 }
 
-function add(elements, text) {
-  function add(Elts, text) {
-    var task = text.trim().split(" ").pop();
-    if (text.trim().split(" ").length == 1) {
-      console.log('"error" no tasks to add!')
-    } else {
-      elements = elements.push(task);
-      Elts = Elts.push({ name: task, done: false });
-      console.log(task + ' has been added to list successfully.')
-    }
+
+/**
+ * * edit
+ * 
+ * @returns {void}
+ */
+function edit(Elts, text) {
+  var tasks = text.trim().split(" ");
+  if (tasks.length == 1) {
+    console.log('"error" no task to edit!')
+  } else if (tasks.length == 2) {
+    Elts[Elts.length - 1].name = tasks[1];
+    console.log('the task ' + Elts.length + ' change to ' + tasks[1])
+  } else {
+    Elts[tasks[1] - 1].name = tasks[2];
+    console.log('the task ' + tasks[1] + ' change to ' + tasks[2])
   }
 }
 
-function edit(elements, text) {
-  function edit(Elts, text) {
-    var tasks = text.trim().split(" ");
-    if (tasks.length == 1) {
-      console.log('"error" no task to edit!')
-    } else if (tasks.length == 2) {
-      elements[elements.length - 1] = tasks[1];
-      console.log('the task ' + elements.length + ' change to ' + tasks[1])
-      Elts[Elts.length - 1].name = tasks[1];
-      console.log('the task ' + Elts.length + ' change to ' + tasks[1])
-    } else {
-      elements[tasks[1] - 1] = tasks[2];
-      Elts[tasks[1] - 1].name = tasks[2];
-      console.log('the task ' + tasks[1] + ' change to ' + tasks[2])
-    }
-  }
-}
 
-function remove(elements, text) {
-  function remove(Elts, text) {
-    var task = text.trim().split(" ").pop();
-    if (text.trim().split(" ").length == 1) {
-      elements.pop();
-      console.log('task ' + elements.length + ' has been removed from list successfully.')
-    } else if (task > elements.length) {
-      Elts.pop();
-      console.log('task ' + Elts.length + ' has been removed from list successfully.')
-    } else if (task > Elts.length) {
-      console.log('this task number is not exist! ')
-    } else {
-      elements.splice(task - 1, 1);
-      Elts.splice(task - 1, 1);
-      console.log('task ' + task + ' has been removed from list successfully.')
-    }
-  }
-}
-
-  function list(elements) {
-    function list(Elts) {
-      console.log('List of commands :')
-      for (i = 1; i <= elements.length; i++) {
-        console.log(i + '- ' + elements[i - 1] + '.')
-      for (var i = 0; i < Elts.length; i++) {
-        if (Elts[i].done) {
-          console.log("[✓] " + Elts[i].name)
-        }
-        else {
-          console.log("[ ] " + Elts[i].name)
-        }
-      }
-    }
-  }
-}
+/**
+ * check
+ * 
+ * @returns {void}
+ */
 function check(Elts, text) {
   var tasks = text.trim().split(" ");
   if (tasks.length == 1) {
@@ -252,6 +183,13 @@ function check(Elts, text) {
     console.log('checked successfully')
   }
 }
+
+
+/**
+ * uncheck
+ * 
+ * @returns {void}
+ */
 function uncheck(Elts, text) {
   var tasks = text.trim().split(" ");
   if (tasks.length == 1) {
@@ -266,6 +204,57 @@ function uncheck(Elts, text) {
   }
 }
 
+
+/**
+ * * remove
+ * 
+ * @returns {void}
+ */
+function remove(Elts, text) {
+  var task = text.trim().split(" ").pop();
+  if (text.trim().split(" ").length == 1) {
+    Elts.pop();
+    console.log('task ' + Elts.length + ' has been removed from list successfully.')
+  } else if (task > Elts.length) {
+    console.log('this task number is not exist! ')
+  } else {
+    Elts.splice(task - 1, 1);
+    console.log('task ' + task + ' has been removed from list successfully.')
+  }
+}
+
+
+/**
+ * * Command to list all tasks
+ * 
+ * @returns {void}
+ */
+function help(elements) {
+  console.log('The possible commands : \n     ' + elements + '.')
+}
+
+
+/**
+ * Exits the application
+ *
+ * @returns {void}
+ */
+function quit(proce) {
+  var fs = require('fs');
+  var data = JSON.stringify(Elts);
+  fs.writeFileSync('database.json', data);
+  if (proce.length > 2) {
+    fs.writeFile('./' + proce[2], data, (err) => {
+      if (!err) {
+        console.log('done');
+      }
+    });
+    fs.writeFileSync(proce[2], data);
+  }
+  console.log('data saved successfully')
+  console.log('Quitting now, goodbye!')
+  process.exit();
+}
 
 // The following line starts the application
 startApp("Aymie Chalouhy")
